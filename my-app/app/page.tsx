d84fbad66c92b2cea2a2549de914bb72c2e9d51f
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2024-02-20 13:51:43 -0500

    set-up

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="">
+ <h1>hi</h1>
+ <Link href="/users">users</Link>
+    </main>
+  );
+}
